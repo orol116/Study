6d3357b1e8ce9536ba3cf889ea79d2b5e9276d76
주빈 <orol116@naver.com>
2024-01-17 17:37:30 +0900

    [study] Spring Security, JWT 설정 (2) - config 재설정 및 회원가입 안되는 현상 개선 필요

--- api/src/main/java/com/rest/api/config/security/SecurityConfiguration.java
+++ api/src/main/java/com/rest/api/config/security/SecurityConfiguration.java
@@ -2,43 +2,61 @@ package com.rest.api.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>  {
+public class SecurityConfiguration {
 
 	private final JwtTokenProvider jwtTokenProvider;
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
-		http.csrf(AbstractHttpConfigurer::disable)
-			// disable session
-	        .sessionManagement((sessionManagement) ->
-	                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	        		)
-	        .authorizeHttpRequests((authorizeRequests)->
-		            authorizeRequests
-	                    //users 포함한 end point 보안 적용 X
-	                    .requestMatchers("/v2/api-docs").permitAll() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다.
-	                    .requestMatchers("/swagger-resources/**").permitAll()
-	                    .requestMatchers("/swagger-ui.html").permitAll()
-	                    .requestMatchers("/webjars/**").permitAll()
-	                    .requestMatchers("/swagger/**").permitAll()
-	                    .anyRequest().authenticated() // 그 외 인증 없이 접근X
-	        		);
-		
-		return http.build();
-	}
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		
+//		http.csrf(AbstractHttpConfigurer::disable)
+//			// disable session
+//	        .sessionManagement((sessionManagement) ->
+//	                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//	        		)
+//	        .authorizeHttpRequests((authorizeRequests)->
+//		            authorizeRequests
+//	                    .requestMatchers("/v2/api-docs").permitAll() // HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
+//	                    .requestMatchers("/swagger-resources/**").permitAll()
+//	                    .requestMatchers("/swagger-ui.html").permitAll()
+//	                    .requestMatchers("/webjars/**").permitAll()
+//	                    .requestMatchers("/swagger/**").permitAll()
+//	                    .anyRequest().authenticated() // 그 외 인증 없이 접근X
+//	        		);
+//		
+//		return http.build();
+//	}
+	
+    @Bean
+    public SecurityFilterChain filterChain(final @NotNull HttpSecurity http) throws Exception {
+        http.httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**", "/swagger-ui/**").permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                );
+        return http.build();
+    }
 	
 }
